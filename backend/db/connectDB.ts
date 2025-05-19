039ce60e9dba6da62bd7a096e954c0a83d92a0e2
seobisback <seobisback@gmail.com>
2025-05-19 13:57:25 +0900

    fix: connectDB 콘솔 로그, catch 스코프 리턴 제거

--- backend/db/connectDB.ts
+++ backend/db/connectDB.ts
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
+import chalk from "chalk";
 import { MONGO_URI } from "../config";
 
 const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
-        console.log("MongoDB connected");
+        console.log(chalk.blue('[MongoDB]:'), chalk.green('MongoDB 데이터베이스 연결 성공'));
         return mongoose.connection;
     } catch (error) {
-        console.error("MongoDB connection failed:", error);
+        console.error(chalk.red('[MongoDB]:'), chalk.red('MongoDB 데이터베이스 연결 실패'), error);
         process.exit(1);
-        return null;
     }
 };
 
