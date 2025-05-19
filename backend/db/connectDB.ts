c40485c6de42f7e653a3e3cbd12ef6138b2d5de3
seobisback <seobisback@gmail.com>
2025-05-19 13:41:22 +0900

    Update connectDB.ts

--- backend/db/connectDB.ts
+++ backend/db/connectDB.ts
@@ -5,9 +5,11 @@ const connectDB = async () => {
     try {
         await mongoose.connect(MONGO_URI);
         console.log("MongoDB connected");
+        return mongoose.connection;
     } catch (error) {
         console.error("MongoDB connection failed:", error);
         process.exit(1);
+        return null;
     }
 };
 
