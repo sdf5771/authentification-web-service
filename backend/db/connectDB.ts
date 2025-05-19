0f3151dcfbea28eb00ab2cfccfaf54ef0d124cee
seobisback <seobisback@gmail.com>
2025-05-19 13:39:13 +0900

    feat: connectDB

---
+++ backend/db/connectDB.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { MONGO_URI } from "../config";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
