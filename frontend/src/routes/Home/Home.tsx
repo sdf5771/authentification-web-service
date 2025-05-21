4e826575df5b27dde0ee03e6a6057e22e420bb8a
seobisback <seobisback@gmail.com>
2025-05-21 09:34:58 +0900

    init: frontend project

---
+++ frontend/src/routes/Home/Home.tsx
@@ -0,0 +1,13 @@
+import styles from './Home.module.css';
+
+function Home() {
+    
+    return (
+        <main className={styles.page_root}>
+            <h1>Home</h1>
+            <h2>You are logged in</h2>
+        </main>
+    )
+}
+
+export default Home;
